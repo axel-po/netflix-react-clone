2ca78dcd1a1e7462c47c3e4f05ac64bfb74a6562
Axel Po <axelpoin@gmail.com>
2022-05-09 22:16:25 +0200

    fix ;

--- src/components/LoadingFullScreen/LoadingFullScreen.js
+++ src/components/LoadingFullScreen/LoadingFullScreen.js
@@ -3,7 +3,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 export default function LoadingFullScreen() {
   return (
     <div className='flex h-[100vh] items-center justify-center'>
-      <CircularProgress />;
+      <CircularProgress />
     </div>
   );
 }
