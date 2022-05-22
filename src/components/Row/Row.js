d4114dc72a114089261b1ea22c6a6a07075e6933
Axel Po <axelpoin@gmail.com>
2022-05-22 13:35:38 +0200

    fix row slider

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,23 +5,31 @@ import { Link } from "react-router-dom";
 import { buildImagePath } from "../../utils/helper";
 import { Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useWidth } from "../../utils/hooks";
 
 import "swiper/css";
 import "swiper/css/navigation";
 
 export default function Row({ title = "Films", type = TYPE_MOVIE, filter = "populaire", param, mt }) {
   const data = useMovieFilter(type, filter, param);
+  const width = useWidth();
 
   if (!data) {
-    return <RowSkeleton title={title} />;
+    return <RowSkeleton title={title} nbSlide={width <= 500 ? 2 : width <= 900 ? 3 : width <= 1400 ? 4 : width >= 1400 ? 5 : 5} />;
   }
 
   return (
     <section className='px-4 sm-2xl:px-10' style={{ marginTop: mt }}>
-      <h2 className='text-md text-white sm:text-xl font-bold mt-8 mb-5'>{title}</h2>
-      <Swiper modules={[Navigation, Pagination]} loop={true} slidesPerView={4} slidesPerGroup={4} navigation spaceBetween={10}>
+      <h2 className='text-md font-bold text-white sm:text-xl  sm:mt-8 sm:mb-2'>{title}</h2>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        loop={true}
+        slidesPerView={width <= 500 ? 2 : width <= 900 ? 3 : width <= 1400 ? 4 : width >= 1400 ? 5 : 5}
+        slidesPerGroup={width <= 500 ? 2 : width <= 900 ? 3 : width <= 1400 ? 4 : width >= 1400 ? 5 : 5}
+        navigation={width >= 650 && true}
+        spaceBetween={10}>
         {data.map((movie) => (
-          <SwiperSlide>
+          <SwiperSlide key={movie.id}>
             <Link to={`/${type}/${movie?.id}`}>
               <img className='rounded-md hover:scale-y-105' src={buildImagePath(movie)} alt={movie?.title} />
             </Link>
--- src/components/skeletons/RowSkeleton.js
+++ src/components/skeletons/RowSkeleton.js
@@ -1,33 +1,42 @@
-import { useState } from "react";
 import Skeleton from "@mui/material/Skeleton";
 import { useWidth } from "../../utils/hooks";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default function RowSkeleton({ nbElement = 20, title = "Films" }) {
+export default function RowSkeleton({ nbElement = 20, title = "Films", nbSlide = 3 }) {
   const styles = {
     row: {
       backgroundColor: "#2C2C2C",
     },
   };
+  const width = useWidth();
 
   const postersSkeletons = [];
 
-  const width = useWidth();
-
   for (let i = 0; i < nbElement; i++) {
-    postersSkeletons.push(
-      <div style={styles.row} key={i} className='rounded-sm w-6/12 shrink-0 max-w-xs cursor-pointer transition-all ease-out hover:scale-110'>
-        {width <= 683 ? (
-          <Skeleton variant='rectangular' height={120} animation='wave' />
-        ) : (
-          <Skeleton variant='rectangular' height={170} animation='wave' />
-        )}
-      </div>
-    );
+    postersSkeletons.push(i);
   }
+
   return (
-    <section className='px-4 sm-2xl:px-14'>
+    <section className='px-4 sm-2xl:px-10'>
       <h2 className='text-md text-white sm:text-xl font-bold mt-8 mb-5'>{title}</h2>
-      <div className='flex gap-2 overflow-y-hidden overflow-x-auto mb-10'>{postersSkeletons}</div>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        loop={true}
+        slidesPerView={nbSlide}
+        slidesPerGroup={nbSlide}
+        navigation={width >= 650 && true}
+        spaceBetween={10}>
+        {postersSkeletons.map((skeletons, id) => (
+          <SwiperSlide key={id} style={styles.row}>
+            {width <= 683 ? (
+              <Skeleton variant='rectangular' height={120} animation='wave' />
+            ) : (
+              <Skeleton variant='rectangular' height={170} animation='wave' />
+            )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 }
